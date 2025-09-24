8e446e799078972ab2544ab995450831bbb0f0b8
naveenroy65 <albertjay112@gmail.com>
2025-09-24 21:19:07 +0530

    Add files via upload

---
+++ Java-CICD-Demo_1/src/test/java/com/demo/AppTest.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testAdd() {
+        App app = new App();
+        assertEquals(5, app.add(2, 3), "2 + 3 should equal 5");
+    }
+}
